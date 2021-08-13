689c0da518ac9bf6fdca2cf5e3c4a78a7b2d3ee9
peter-madigan <pmadigan@berkeley.edu>
2021-08-13 11:41:35 -0700

    Fix pytest fixture

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,11 +1,17 @@
 import pytest
 import os
 
-from h5flow import run
+from h5flow import run, H5FLOW_MPI
 
-@pytest.fixture
-def testfile(mpi_tmp_path):
-    return os.path.join(mpi_tmp_path, 'test.h5')
+if H5FLOW_MPI:
+    @pytest.fixture
+    def testfile(mpi_tmp_path):
+        return os.path.join(mpi_tmp_path, 'test.h5')
+else:
+    @pytest.fixture
+    def testfile(tmp_path):
+        return os.path.join(tmp_path, 'test.h5')
+
 
 def test_example(testfile):
     run('example_config.yaml', testfile, verbose=2)
