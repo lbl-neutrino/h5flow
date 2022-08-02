ece5c00ca43f972d5d3d8d3415069d8c24b85a2e
peter-madigan <pmadigan@berkeley.edu>
2022-08-01 19:38:39 -0700

    Fix batching mechanism and fix reference regeneration

--- scripts/h5flow_extract.py
+++ scripts/h5flow_extract.py
@@ -27,7 +27,7 @@ def main(**kwargs):
         # create a copied file
         with H5FlowDataManager(output_filename, 'a') as f_out:
 
-            print('Preparing selection')
+            print('Preparing selection') if f_out.rank == 0 else None
             for idset,dset in enumerate(datasets):
                 # mark all entries that should be copied
                 if entry_filter is not None:
@@ -35,32 +35,32 @@ def main(**kwargs):
 
                     # default - select all
                     if filter_ is None:
-                        entries = np.r_[slice(f_in.get_dset(dset).shape[0])]
+                        entries = np.r_[slice(f_in.get_dset(dset).shape[0])].astype(int)
                     
                     # check if specified by a list of entries
                     elif isinstance(filter_[0], int):
-                        entries = filter_
+                        entries = np.array(filter_, dtype=int)
 
                     # specified by a dataset within file
                     else:
                         filter_dset = f_in.get_dset(filter_[0])[:]
                         if filter_[1] is not None:
-                            entries = np.where(filter_dset[filter_[1]].astype(bool))[0]
+                            entries = np.where(filter_dset[filter_[1]].astype(bool))[0].astype(int)
                         else:
-                            entries = np.where(filter_dset.astype(bool))[0]
+                            entries = np.where(filter_dset.astype(bool))[0].astype(int).tolist()
                 else:
                     raise RuntimeError('No entry selection means specified')    
 
                 to_process.append((dset, entries))
-                print(f'Extract {len(entries)} entries from {dset}')
+                print(f'Extract {len(entries)} entries from {dset}') if f_out.rank == 0 else None
 
-            pbar = tqdm.tqdm(total=len(to_process), position=0)
+            pbar = tqdm.tqdm(total=len(to_process), position=0, disable=f_out.rank != 0)
             while True:
                 if not to_process:
                     break
 
                 dset, entries = to_process[0]
-                pbar.desc = dset + f' ({len(entries)}, {len(to_process)})'
+                pbar.desc = dset + f' {len(entries)} elem, {len(to_process)} dsets remaining'
                 pbar.update()
                 del to_process[0]
                 # already finish processing dataset or empty entry list
@@ -70,35 +70,36 @@ def main(**kwargs):
                 else:
                     processed.append(dset)
 
-                if verbose:
-                    print(f'Marking {dset} ({len(entries)})')
                 flag_dset = dset + '~'
                 # make boolean flag dataset (dset~)
-                if flag_dset not in f_out['/']:
+                if flag_dset not in flag_dsets:
                     flag_dsets.append(flag_dset)
                     f_out.create_dset(flag_dset, dtype=bool)
                     f_out.reserve_data(flag_dset, f_in.get_dset(dset).shape[0])
 
                 # mark entries that should be flagged
-                for ibatch in tqdm.tqdm(range(len(entries)//batch_size + 1), position=1, leave=None, desc='Create flags'):
-                    batch = entries[ibatch * batch_size:(ibatch + 1) * batch_size]
+                batch_size_ = max(batch_size // entries.dtype.itemsize, 1)
+                for ibatch in tqdm.tqdm(range(f_out.rank, len(entries)//batch_size_ + 1 + f_out.rank,  f_out.size), position=1, leave=None, desc=f'Create flags {batch_size_}', disable=f_out.rank != 0):
+                    batch = entries[ibatch * batch_size_:(ibatch + 1) * batch_size_]
                     if not len(batch):
-                        continue                    
+                        batch = slice(-1,-1)
                     f_out.write_data(flag_dset, batch, True)
 
                 # get associated reference datasets
                 refs = f_in.get_refs(dset)
-                for ref in tqdm.tqdm(refs, leave=None, position=1, desc='Get referred entries'):
-                    if verbose:
-                        print(f'Marking reference {ref.name}')
+                for ref in tqdm.tqdm(refs, leave=None, position=2, desc='Collect referred dsets', disable=f_out.rank != 0):
                     # add referred data to processing queue
                     other_dset = ref.attrs['dset1'][1:-5] if ref.attrs['dset0'][1:-5] == dset else ref.attrs['dset0'][1:-5]
+                    
                     if other_dset in processed:
                         continue
 
-                    child_entries = list()
-                    for ibatch in tqdm.tqdm(range(len(entries)//batch_size + 1), position=2, leave=None, desc=f'{ref.name}'):
-                        batch = entries[ibatch * batch_size:(ibatch + 1) * batch_size]
+                    child_entries = [list()]
+                    ref_region = f_in.get_ref_region(dset, other_dset)
+                    batch_size_ = max(batch_size // int(np.max((ref_region[:batch_size]['stop'] - ref_region[:batch_size]['start']) * 2 * ref.dtype.itemsize).clip(1,None)), 1)
+                    for ibatch in tqdm.tqdm(range(f_out.rank, len(entries)//batch_size_ + f_out.rank + 1, f_out.size), position=3, leave=None, desc=f'{ref.name} {batch_size_}', disable=f_out.rank != 0):
+                        batch = entries[ibatch * batch_size_:(ibatch + 1) * batch_size_]
+
                         if not len(batch):
                             continue
 
@@ -106,75 +107,115 @@ def main(**kwargs):
                         ref_region = f_in.get_ref_region(dset, other_dset)[offset:batch.max()+1]
                         ref_region = ref_region[batch - offset]
                         ref_region = ref_region[ref_region['start'] != ref_region['stop']]
+
                         if not len(ref_region):
                             continue
                         subset = slice(ref_region['start'].min(), ref_region['stop'].max())
                         
                         # mark data referred by entries
-                        ref_dset,ref_dir = f_in.get_ref(dset, other_dset)                        
-                        ref_entries = np.where(np.isin(ref[subset, ref_dir[0]], batch))[0]
+                        ref_dset,ref_dir = f_in.get_ref(dset, other_dset)
+                        ref_entries = np.where(np.isin(ref[subset, ref_dir[0]], batch))[0].astype(int)
                         if not len(ref_entries):
                             continue
-
                         # add children and entries to list of datasets to process
-                        child = ref.attrs['dset0'][1:-5] if ref_dir[0] == 1 else ref.attrs['dset1'][1:-5]
                         offset = ref_entries.min()
                         child_batch_entries = np.unique(ref_dset[subset][ref_entries,ref_dir[1]]).astype(int)
                         child_entries.append(child_batch_entries)
+                    if h5flow.H5FLOW_MPI:
+                        child_entries = f_out.comm.allgather(child_entries)
+                        child_entries = [v for vs in child_entries for v in vs]
 
                     if len(child_entries):
-                        child_entries = np.sort(np.unique(np.concatenate(child_entries, axis=0)))
+                        child_entries = np.sort(np.unique(np.concatenate(child_entries, axis=0))).astype(int)
                         if len(child_entries):
-                            if verbose:
-                                print(f'Adding {child} with {len(child_entries)}')
-                            to_process.append((child, child_entries))
+                            if verbose and f_out.rank == 0:
+                                print(f'Adding {other_dset} with {len(child_entries)}')
+                            to_process.append((other_dset, child_entries))
                             pbar.total += 1
                             pbar.update(0)
-
-            if verbose:
+            if f_out.rank == 0:
+                print()
+            if verbose and f_out.rank == 0:
                 print('Flag datasets:', flag_dsets)
 
             # copy marked data to output file
             def copy_data(path, obj):
                 if isinstance(obj, h5py.Group):
                     if len(obj.attrs):
-                        if verbose:
+                        if verbose and f_out.rank == 0:
                             print(f'Copy attributes on {path}')
                         f_out.set_attrs(path, **dict(obj.attrs))
                     return
 
-                name = path.removeprefix('/').removesuffix('/data').removesuffix('/ref').removesuffix('/ref_region')
                 if path.endswith('/data'):
                     # make resizable dataset of correct type / shape
+                    name = path[:-5]
                     flag_dset_name = name + '~'
                     if flag_dset_name not in flag_dsets:
                         return
-                    if verbose:
+                    if verbose and f_out.rank == 0:
                         print(f'Copy data from {path}')
                     flag_dset = f_out.get_dset(flag_dset_name)
                     f_out.create_dset(name, dtype=obj.dtype, shape=obj.shape[1:])
                         
                     # copy entries
-                    flag_index = np.where(flag_dset)[0]
-                    for ibatch in tqdm.tqdm(range(len(flag_index)//batch_size + 1), desc=f'Copy {path}'):
-                        batch = flag_index[ibatch * batch_size:(ibatch + 1) * batch_size]
-                    
-                        out_slice = f_out.reserve_data(name, len(batch))
-                        offset = batch.min()
-                        f_out.write_data(name, out_slice, obj[offset:batch.max()+1][batch-offset])
+                    batch_size_ = max(batch_size // obj.dtype.itemsize, 1)
+                    flag_batch_size_ = max(batch_size // flag_dset.dtype.itemsize, 1)
+                    pbar = tqdm.tqdm(total=len(flag_dset), desc=f'Copy {path} {batch_size_}', disable=f_out.rank != 0)
+                    for ibatch_flag in range(len(flag_dset)//flag_batch_size_ + 1):
+                        flag_batch = flag_dset[ibatch_flag * flag_batch_size_:(ibatch_flag+1) * flag_batch_size_]
+                        flag_index = np.where(flag_batch)[0].astype(int) + ibatch_flag * flag_batch_size_
+
+                        for ibatch in range(f_out.rank, len(flag_index)//batch_size_ + f_out.rank + 1, f_out.size):
+                            batch = flag_index[ibatch * batch_size_:(ibatch + 1) * batch_size_]
+                            out_slice = f_out.reserve_data(name, len(batch))
+                            copy_data = np.empty((0,)+obj.shape[1:], dtype=obj.dtype)
+                            if len(batch):
+                                offset = batch.min()
+                                copy_data = obj[offset:batch.max()+1][batch-offset]
+                            f_out.write_data(name, out_slice, copy_data)
+                        pbar.update(len(flag_batch))
 
                     # copy attributes
-                    if verbose:
+                    if verbose and f_out.rank == 0:
                         print(f'Copy attributes on {path}')
                     f_out.set_attrs(path, **dict(obj.attrs))
                     return
                 return
 
+            def calc_new_index(idx, flag_dset, cache=None):
+                # ``idx`` is a 1d index array marking the indices we want to convert
+                # ``flag_dset`` is a boolean dataset marking entries that should be counted towards new indexing
+                # ``cache`` is a stored old index, new index mapping
+                #
+                # returns an array of the same shape as ``idx``, but with values representing their position within a masked array: ``arr[flag_dset][return_idx] == arr[idx]``
+                if cache is None:
+                    cache = [(0,0)]
+                if len(idx):
+                    min_idx = idx.min()
+
+                    cache_entry = cache[-1]
+                    while min_idx < cache_entry[0]:
+                        if cache:
+                            del cache[-1]
+                        if cache:
+                            cache_entry = cache[-1]
+                        else:
+                            cache_entry = (0,0)
+                            break
+                            
+                    offset = min(min_idx, cache_entry[0])
+                    batch = flag_dset[offset:idx.max()+1].astype(int)
+                    new_idx = (np.cumsum(batch)-1 + cache_entry[1])[idx-offset]
+
+                    cache.append((idx.max(), new_idx.max()))
+                    return new_idx, cache
+                return np.zeros_like(idx), cache
+                        
             def regen_ref(path, obj):
                 if isinstance(obj, h5py.Group):
                     return
             
-                name = path.removeprefix('/').removesuffix('/data').removesuffix('/ref').removesuffix('/ref_region')
                 if path.endswith('/ref'):
                     # make resizable dataset of correct type / shape
                     parent_dset_name = obj.attrs['dset0'][1:-5]
@@ -185,7 +226,7 @@ def main(**kwargs):
                     child_flag_dset_name = child_dset_name + '~'
                     if any([dset_name not in flag_dsets for dset_name in (parent_flag_dset_name, child_flag_dset_name)]):
                         return
-                    if verbose:
+                    if verbose and f_out.rank == 0:
                         print(f'Regenerating references from {path}')
 
                     # create reference dataset
@@ -193,53 +234,51 @@ def main(**kwargs):
                         
                     parent_flag_dset = f_out.get_dset(parent_dset_name + '~')
                     child_flag_dset = f_out.get_dset(child_dset_name + '~')
-                    parent_flag_index = np.where(parent_flag_dset)[0]
-                    child_flag_index = np.where(child_flag_dset)[0]                    
-                    parent_new_index = np.cumsum(parent_flag_dset)-1
-                    child_new_index= np.cumsum(child_flag_dset)-1
 
                     # copy only references with both parent and child index
                     ref, ref_dir = f_in.get_ref(parent_dset_name, child_dset_name)
-                    ref_region_parent = f_in.get_ref_region(parent_dset_name, child_dset_name)
-                    for ibatch in tqdm.tqdm(range(len(parent_flag_index)//batch_size + 1), desc=f'Regenerate {path}'):
-                        batch_indices = parent_flag_index[ibatch * batch_size:(ibatch + 1) * batch_size]
-                        if not len(batch_indices):
-                            continue
 
-                        offset = batch_indices.min()
-                        regions = ref_region_parent[offset:batch_indices.max()+1]
-                        regions = regions[batch_indices - offset]
-                        regions = regions[regions['start'] != regions['stop']]
-                        if not len(regions):
-                            continue
-
-                        batch_ref = ref[regions['start'].min():regions['stop'].max()]
-                        ref_sel = np.where(np.isin(batch_ref[:,ref_dir[0]], batch_indices) & np.isin(batch_ref[:,ref_dir[1]], child_flag_index))[0]
-                        new_ref0 = parent_new_index[batch_ref[ref_sel,ref_dir[0]]]
-                        new_ref1 = child_new_index[batch_ref[ref_sel,ref_dir[1]]]
-
-                        new_ref = np.c_[new_ref0, new_ref1]
+                    batch_size_ = max(batch_size // int(max(2 * obj.dtype.itemsize * f_out.size, 1)), 1)
+                    parent_cache = None
+                    child_cache = None
+                    for ibatch in tqdm.tqdm(range(f_out.rank, len(ref)//batch_size_ + f_out.rank + 1, f_out.size), desc=f'Regen {path} {batch_size_}', disable=f_out.rank != 0):
+                        batch_ref = ref[ibatch*batch_size_:(ibatch+1)*batch_size_]
+                        new_ref = batch_ref.copy().astype(int)
+                        if len(new_ref):
+                            offset = new_ref[:,ref_dir[0]].min()
+                            end = new_ref[:,ref_dir[0]].max()+1
+                            batch_parent_flag = parent_flag_dset[offset:end][new_ref[:,ref_dir[0]]-offset].astype(bool)
+                            new_ref = new_ref[batch_parent_flag]
+                            if len(new_ref):
+                                offset = new_ref[:,ref_dir[1]].min()
+                                end = new_ref[:,ref_dir[1]].max()+1
+                                batch_child_flag = child_flag_dset[offset:end][new_ref[:,ref_dir[1]]-offset].astype(bool)
+                                new_ref = new_ref[batch_child_flag]
+                                if len(new_ref):
+                                    new_ref[:,ref_dir[0]], parent_cache = calc_new_index(new_ref[:,ref_dir[0]], parent_flag_dset, cache=parent_cache)
+                                    new_ref[:,ref_dir[1]], child_cache = calc_new_index(new_ref[:,ref_dir[1]], child_flag_dset, cache=child_cache)
                         f_out.write_ref(parent_dset_name, child_dset_name, new_ref)
 
                     # copy attributes
-                    f_out.set_attrs(path, **dict(obj.attrs))            
+                    f_out.set_attrs(path, **dict(obj.attrs))
                     return
                 return
 
-            print('Copying data')
+            print('Copying data') if f_out.rank == 0 else None
             f_in['/'].visititems(copy_data)
-            print('Regenerating references')
+            print('Regenerating references') if f_out.rank == 0 else None
             f_in['/'].visititems(regen_ref)
 
-            print('Deleting flag datasets')
+            print('Deleting flag datasets') if f_out.rank == 0 else None
             for path in flag_dsets:
                 if path in f_out['/']:
-                    if verbose:
+                    if verbose and f_out.rank == 0:
                         print(f'Delete {path}')
                     f_out.delete(path)
 
-            print('Extracted data:')
-            f_out['/'].visititems(lambda n,o: print(n, o.shape) if isinstance(o, h5py.Dataset) and (n.endswith('/data') or n.endswith('/ref') or n.endswith('/ref_region')) else None)
+            if f_out.rank == 0:
+                print('Extracted data:')
+                f_out['/'].visititems(lambda n,o: print(n, o.shape) if isinstance(o, h5py.Dataset) and (n[-5:] == '/data' or n[-4:] == '/ref' or n[-11:] == '/ref_region') else None)
 
                 
 if __name__ == '__main__':
@@ -254,8 +293,8 @@ if __name__ == '__main__':
                         help='''Datasets to copy''')
     parser.add_argument('--filter', '-f', type=json.loads, default=None,
                         help='''JSON-formatted filter spec for each dataset''')
-    parser.add_argument('--batch_size', type=int, default=1024,
-                        help='''Maximum entries to load at a given time''')
+    parser.add_argument('--batch_size', type=int, default=1024*1024,
+                        help='''Maximum data to handle at a given time (bytes)''')
     # filter spec:
     # [
     #   (dataset, boolean field),
